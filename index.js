0e1cc801f1e497fa56c03a82eb1e4535a0eb1534
fida-ur-rehman <55063076+fida-ur-rehman@users.noreply.github.com>
2020-06-12 00:57:41 +0530

    1

--- index.js
+++ index.js
@@ -620,7 +620,9 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = process.env.PORT || 3000;
 }
-app.listen(port);
+app.listen(port , function(){
+  console.log("Server HAs Started Successfully")
+});
 
 // functions
 
